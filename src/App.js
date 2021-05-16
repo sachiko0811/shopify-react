455857cbd184462a4b2da9a3a0763a73f03887f2
sachiko0811 <sachikonovel@yahoo.co.jp>
2021-05-16 15:03:15 -0700

    Footer

--- src/App.js
+++ src/App.js
@@ -6,24 +6,25 @@ import ProductPage from './pages/ProductPage';
 import NavBar from './components/NavBar';
 import Cart from './components/Cart';
 import NavMenu from './components/NavMenu';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <NavBar />
-        <Cart />
-        <NavMenu />
-        <Switch>
-          <Route path="/products/:handle">
-            <ProductPage />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-        <p>Footer</p>
-      </Router>
+    <Router>
+      <NavBar />
+      <Cart />
+      <NavMenu />
+      <Switch>
+        <Route path="/products/:handle">
+          <ProductPage />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+      <Footer />
+     </Router>
     </div>
   );
 }
