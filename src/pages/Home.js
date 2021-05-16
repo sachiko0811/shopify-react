2e70cf0029e7e8c90c52fe419568b0eabaa3a28e
sachiko0811 <sachikonovel@yahoo.co.jp>
2021-05-16 15:18:05 -0700

    Hero

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,8 @@ import { Box, Grid, Text, Image } from '@chakra-ui/react';
 
 import { ShopContext } from '../context/shopContext';
 
+import Hero from '../components/Hero';
+
  const Home = () => {
 
   const { fetchAllProducts, products } = useContext(ShopContext)
@@ -18,6 +20,7 @@ import { ShopContext } from '../context/shopContext';
 
   return (
     <Box>
+      <Hero />
       <Grid templateColumns="repeat(3, 1fr)">
       {
         products.map(product => (
